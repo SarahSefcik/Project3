d2441158c639796c05ac625bcb39e9b1fafbee2a
Sarah Catherine Sefcik <sefciksa@gmail.com>
2019-02-06 20:19:26 -0500

    comments in the app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+// Require express, path, cookieParser, bodyParser
+
+// encryption
+
+// create token
+
+// Middleware
+
+// Require model for syncing
+
+// using the header with a content-type of 'Authorization'
+
+// node server setup so requests and responses can be parsed
+
+// set up an express jsonwebtoken middleware(simply required for express to properly utilize the token for requests) instantiate this with the same secret that will be sent to the client
+
+// route that the client will pass the entered credentials for verification to. If credentials match, the server sends back a json response with a valid json web token for the client to use for identification
+
